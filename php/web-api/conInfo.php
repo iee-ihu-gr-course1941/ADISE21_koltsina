bd3fedb9a0a79d3b81427f6b58f9e1b463c0fa56
Orfous <53450327+Orfous@users.noreply.github.com>
2022-01-14 20:24:46 +0200

    Addendum change host 8

--- php/web-api/conInfo.php
+++ php/web-api/conInfo.php
@@ -1,15 +1,13 @@
 <?php
-    $host='https://users.iee.ihu.gr';
-    $user='root';
-    $pass='1234'; // αυτό που ορίσατε κατά το create
-    $db = 'koltsinadb';
+$user='root';
+$pass='1234';
+$host='localhost';
+$db = 'THE_DB';
 
-    $mysqli = new mysqli($host, $user, $pass, $db);
-    // $mysqli = new mysqli($host, $user, $pass, $db, null,
-    // '/path to home directory/mysql/run/mysql.sock');
-    
-    if ($mysqli -> connect_errno) {
-        echo "Failed to connect to MySQL: (" .
-            $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }
-?>
+
+$mysqli = new mysqli($host, $user, $pass, $db,null,'~/public_html/ADISE21_koltsina/mysql/run/mysql.sock');
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" .
+        $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}?>
+
