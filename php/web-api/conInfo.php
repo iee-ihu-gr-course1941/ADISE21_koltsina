dad5364e32fd7bdc40693a04603588cd9986fb1e
Orfous <53450327+Orfous@users.noreply.github.com>
2022-01-14 20:27:07 +0200

    Addendum change host 9

--- php/web-api/conInfo.php
+++ php/web-api/conInfo.php
@@ -5,7 +5,7 @@ $host='localhost';
 $db = 'THE_DB';
 
 
-$mysqli = new mysqli($host, $user, $pass, $db,null,'~/public_html/ADISE21_koltsina/mysql/run/mysql.sock');
+$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174951/mysql/run/mysql.sock');
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" .
         $mysqli->connect_errno . ") " . $mysqli->connect_error;
