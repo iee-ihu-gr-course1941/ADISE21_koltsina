1416aaaf5b314238bad022d6ef55d0abb957f76b
helenkoll <57528987+helenkoll@users.noreply.github.com>
2022-01-15 17:07:16 +0200

    Fixxing Errors

--- php/web-api/addPlayer.php
+++ php/web-api/addPlayer.php
@@ -1,25 +1,26 @@
 <?php
 
-require_once
-    php/web-api/conInfo.php;
+    include('./conInfo.php');
 
-$username =  $_POST['username'];
-$player_no = $_POST['player_no'];
-$game_id =  $_POST['game_id'];
+    $username =  $_POST['username'];
+    $player_no = $_POST['player_no'];
+    $game_id =  $_POST['game_id'];
 
-$query = "INSERT INTO Players (username, player_no, game_id) VALUES ('$username', $player_no, $game_id);";
-$mysqli ="";
-$stmt = $mysqli-> query($query);
-$query = "SELECT * FROM Players WHERE player_no = $player_no AND game_id = $game_id;";
-$stmt = $mysqli -> query($query);
-$row = $stmt->fetch_assoc();
+    $query = "INSERT INTO Players (username, player_no, game_id) VALUES ('$username', $player_no, $game_id);";
+    echo $query;
+    // $stmt = $mysqli -> query($query);
 
-$player_data = new \stdClass();
-$player_data->id = $row['id'];
-$player_data->username = $row['username'];
-$player_data->player_no = $row['player_no'];
-$player_data->game_id = $row['game_id'];
+    $query = "SELECT * FROM Players WHERE player_no = $player_no AND game_id = $game_id;";
+    echo $query;
+    // $stmt = $mysqli -> query($query);
+    // $row = $stmt->fetch_assoc();
 
-$json = json_encode($player_data);
-echo $json;
+    // $player_data = new \stdClass();
+    // $player_data->id = $row['id'];
+    // $player_data->username = $row['username'];
+    // $player_data->player_no = $row['player_no'];
+    // $player_data->game_id = $row['game_id'];
+
+    // $json = json_encode($player_data);
+    // echo $json;
 ?>
