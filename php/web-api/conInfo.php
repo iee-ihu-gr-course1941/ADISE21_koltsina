04fc6918161ebbded4711625e2f78a1a801a3150
helenkoll <57528987+helenkoll@users.noreply.github.com>
2022-01-15 16:31:25 +0200

    Fixxing speeling 2

--- php/web-api/conInfo.php
+++ php/web-api/conInfo.php
@@ -6,7 +6,7 @@ $db = 'koltsinadb';
 
 
 if(gethostname()=='users.iee.ihu.gr') {
-    $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174951/mysql/run/mysql.sock');
+    $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174913/mysql/run/mysql.sock');
 } else {
 //    $pass=null;
     $mysqli = new mysqli($host, $user, $pass, $db);
@@ -15,4 +15,3 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" .
         $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }?>
-
