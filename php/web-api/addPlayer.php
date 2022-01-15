2aec74af890abfe56c16f62fa7325ee449cf1595
helenkoll <57528987+helenkoll@users.noreply.github.com>
2022-01-15 17:18:37 +0200

    Fixxing Errors

--- php/web-api/addPlayer.php
+++ php/web-api/addPlayer.php
@@ -7,20 +7,18 @@
     $game_id =  $_POST['game_id'];
 
     $query = "INSERT INTO Players (username, player_no, game_id) VALUES ('$username', $player_no, $game_id);";
-    echo $query;
-    // $stmt = $mysqli -> query($query);
+    $stmt = $mysqli -> query($query);
 
     $query = "SELECT * FROM Players WHERE player_no = $player_no AND game_id = $game_id;";
-    echo $query;
-    // $stmt = $mysqli -> query($query);
-    // $row = $stmt->fetch_assoc();
+    $stmt = $mysqli -> query($query);
+    $row = $stmt->fetch_assoc();
 
-    // $player_data = new \stdClass();
-    // $player_data->id = $row['id'];
-    // $player_data->username = $row['username'];
-    // $player_data->player_no = $row['player_no'];
-    // $player_data->game_id = $row['game_id'];
+     $player_data = new \stdClass();
+     $player_data->id = $row['id'];
+     $player_data->username = $row['username'];
+     $player_data->player_no = $row['player_no'];
+     $player_data->game_id = $row['game_id'];
 
-    // $json = json_encode($player_data);
-    // echo $json;
+     $json = json_encode($player_data);
+     echo $json;
 ?>
